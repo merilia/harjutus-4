cc6c5260b8b1aec6716bf0ab4dfa6b58aedaefe5
Merili <meriliasu@gmail.com>
2014-09-16 16:12:25 +0300

    Initial commit
    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Andmetüübid II</title>
+</head>
+<body>
+	<h2>Arvutamine murd- ja täisarvudega</h2>
+	<?php echo $float = 3.14; ?><br>
+	<?php echo $float + 7; ?><br>
+	<?php echo 4/3; ?><br>
+	<h2>Murdarvude ümardamine</h2>
+	<?php
+	 $float = 3.14;
+	 echo round($float,1); ?>
+</body>
+</html>
+	
